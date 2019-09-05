d606219175b6bdef774e327a7a760cff9376776d
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2019-09-05 14:05:34 +0700

    Fixed: Mengubah kalimat redaksi pada form

--- application/views/bendahara/pages/aturPemasukan.php
+++ application/views/bendahara/pages/aturPemasukan.php
@@ -59,7 +59,7 @@
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
                           <div class="modal-header">
-                            <h5 class="modal-title" id="hapus-kelas-modal-label">Konfirmasi Hapus Iuran Bulanan</h5>
+                            <h5 class="modal-title" id="hapus-kelas-modal-label">Konfirmasi Hapus Transaksi Pemasukan</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                               <span aria-hidden="true">&times;</span>
                             </button>
